dcacfb51db05c9e494299709fe643784b59ccccc
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-21 14:37:13 +0100

    feat: implementing miss logic

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,11 +1,13 @@
-export class Bowling {
-  frame: string[] = [];
+type Movements = "X" | "/" | "-" | string;
 
-  roll = (pins: string) => {
+export class Bowling {
+  frame: Movements[] = [];
+
+  roll = (pins: Movements) => {
     this.frame.push(pins);
   };
 
-  rollMany = (frames: number, pins: string) => {
+  rollMany = (frames: number, pins: Movements) => {
     for (let i = 0; i < frames; i++) {
       this.frame.push(pins);
     }
@@ -16,12 +18,14 @@ export class Bowling {
     for (let i = 0; i < 10; i++) {
       if (this.frame[i] === "X") {
         score += 10;
-      }
-      if (this.frame[i + 1] === "X") {
-        score += 10;
-      }
-      if (this.frame[i + 2] === "X") {
-        score += 10;
+        if (this.frame[i + 1] === "X") {
+          score += 10;
+        }
+        if (this.frame[i + 2] === "X") {
+          score += 10;
+        }
+      } else {
+        score += parseInt(this.frame[i]);
       }
     }
     return score;
