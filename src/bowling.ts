6a1cc14e57e3026a94aa94cd824d914b8c3c9336
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-21 14:23:31 +0100

    feat: implementing class bowling

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,1 +1,29 @@
-class Bowling {}
+export class Bowling {
+  frame: string[] = [];
+
+  roll = (pins: string) => {
+    this.frame.push(pins);
+  };
+
+  rollMany = (frames: number, pins: string) => {
+    for (let i = 0; i < frames; i++) {
+      this.frame.push(pins);
+    }
+  };
+
+  getScore = () => {
+    let score = 0;
+    for (let i = 0; i < 10; i++) {
+      if (this.frame[i] === "X") {
+        score += 10;
+      }
+      if (this.frame[i + 1] === "X") {
+        score += 10;
+      }
+      if (this.frame[i + 2] === "X") {
+        score += 10;
+      }
+    }
+    return score;
+  };
+}
