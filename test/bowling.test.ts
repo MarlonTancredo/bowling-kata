46aea895f8954fa61d67ce61736e1f29370ec1f9
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-08-21 14:24:47 +0100

    refact: refactoring stike test

--- test/bowling.test.ts
+++ test/bowling.test.ts
@@ -1,76 +1,22 @@
-type Roll = "X" | "/" | "-" | string;
-let frame: string[] = [];
+import { Bowling } from "../src/bowling";
+
+let bowling = new Bowling();
 
 beforeEach(() => {
-  frame = [];
+  bowling = new Bowling();
 });
 
-describe("Testing Bowling", () => {
-  //Arrange
-  const roll = (pins: Roll) => {
-    frame.push(pins);
-  };
-
-  const rollMany = (frames: number, pins: Roll) => {
-    for (let i = 0; i < frames; i++) {
-      frame.push(pins);
-    }
-  };
-
-  const isStrike = (roll: string) => {
-    return roll === "X";
-  };
-
-  const isSpare = (roll: string) => {
-    return roll.includes("/");
-  };
-
-  const getScore = () => {
-    let score = 0;
-    for (let i = 0; i < 10; i++) {
-      if (isStrike(frame[i])) {
-        score += 10;
-        if (isStrike(frame[i + 1])) {
-          score += 10;
-        }
-        if (isStrike(frame[i + 2])) {
-          score += 10;
-        }
-      } else if (isSpare(frame[i])) {
-        score += 10 + parseInt(frame[i]);
-      } else {
-        score += parseInt(frame[i]);
-      }
-    }
-    return score;
-  };
-
-  test("strike", () => {
+//X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30 points = 300
+describe("Strike", () => {
+  test("Should return 300 points if all frames got a strike ", () => {
+    //Arrange
     //Act
-    rollMany(10, "X");
-    roll("X");
-    roll("X");
-    getScore();
+    bowling.rollMany(10, "X");
+    bowling.roll("X");
+    bowling.roll("X");
+    bowling.getScore();
 
     //Assert
-    expect(getScore()).toBe(300);
-  });
-
-  test("miss", () => {
-    //Act
-    rollMany(10, "9-");
-    getScore();
-
-    //Assert
-    expect(getScore()).toBe(90);
-  });
-
-  test("spare", () => {
-    //Act
-    rollMany(10, "5/");
-    getScore();
-
-    //Assert
-    expect(getScore()).toBe(150);
+    expect(bowling.getScore()).toBe(300);
   });
 });
